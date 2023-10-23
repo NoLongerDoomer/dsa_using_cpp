7bda6d044120574e766e9bf6255e1ba026d13bc0
swarup_home_pc <swarup.rajpurohit.EXT@nmims.edu>
2023-10-23 17:51:39 +0530

    hello world

---
+++ src/hello_world.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : dsa_using_cpp.cpp
+// Author      : Swarup Singh Rajpurohit
+// Version     :
+// Copyright   : NA
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!" << endl; // prints Hello World!
+	return 0;
+}
